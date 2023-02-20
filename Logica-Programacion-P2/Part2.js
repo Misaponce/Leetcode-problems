55d850e57b5b8ae304eff9243fd1223ad64ae323
Misaponce <poncemisael@gmail.com>
2023-02-20 17:34:00 +0000

    two more exp added

--- Logica-Programacion-P2/Part2.js
+++ Logica-Programacion-P2/Part2.js
@@ -1,20 +1,53 @@
 //21. Leer tres números enteros de dos dígitos cada uno y determinar en cuál de ellos se encuentra el mayor dígito.
-function digitoMayor(array) {
-    let largestDigit = 0;
-    let numWithLargestDigit;
-    for (let i = 0; i < array.length; i++){
-        let currentNum = array[i];
-        let stringNum = currentNum.toString();
+// function digitoMayor(array) {
+//     let largestDigit = 0;
+//     let numWithLargestDigit;
+//     for (let i = 0; i < array.length; i++){
+//         let currentNum = array[i];
+//         let stringNum = currentNum.toString();
 
-        for (let j = 0; j < stringNum.length; j++){
-            let currentDigit = parseInt(stringNum[j]);
-            if (currentDigit > largestDigit) {
-                largestDigit = currentDigit;
-                numWithLargestDigit = currentNum;
+//         for (let j = 0; j < stringNum.length; j++){
+//             let currentDigit = parseInt(stringNum[j]);
+//             if (currentDigit > largestDigit) {
+//                 largestDigit = currentDigit;
+//                 numWithLargestDigit = currentNum;
+//             }
+//         }
+//     }
+//     return numWithLargestDigit;
+// }
+
+//22. Leer un número entero de tres dígitos y determinar si el primer dígito es igual al último.
+// function comparaDigitos(num) {
+//     return (~~(num / 100)) === (num % 10);
+// }
+
+//23. Leer un número entero de tres dígitos y determinar cuántos dígitos primos tiene.
+function cuantosDigitosPrimos(num) {
+    let numToString = num.toString();
+    let primoContador = 0;
+
+    for (let i = 0; i < numToString.length; i++) {
+        let backToNum = parseInt(numToString[i]);
+        // console.log(backToNum);
+        //Aplicando recursividad con una funcion para determinar que digitos son primos
+
+        function isPrime(d) {
+            if (d < 2) {
+                return false
             }
+
+            for (let j = 2; j <= Math.sqrt(d); j++){
+                if (d % j === 0) {
+                    return false
+                }
+            }
+            return true
         }
+        isPrime(backToNum) === true ? primoContador++ : primoContador--;
+        console.log(primoContador);
     }
-    return numWithLargestDigit;
+    return primoContador <= 0 ? `El entero ${num} no contiene digitos primos` : `El entero ${num} tiene ${primoContador} digitos primos`
 }
 
-console.log(digitoMayor([82,0,21]));
+console.log(cuantosDigitosPrimos(444));
